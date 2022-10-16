a4f318534369254affab3496135ddc10a9b44b1e
vaniamaksimov <vaniamaksimov@gmail.com>
2022-10-16 21:55:11 +0400

    fix

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py collectstatic --noinput && python manage.py migrate
+python manage.py collectstatic --noinput && python manage.py migrate app cart users
 gunicorn --bind 0:8000 --workers=5 --threads=2 foodgram.wsgi:application
